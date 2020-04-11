07c4b0a9e6a53116ed63872806084682f06a3090
E-351 <mooncake.industrial@gmail.com>
2020-04-11 22:07:45 +0200

    add simple build script

--- fb/build.ps1
+++ fb/build.ps1
@@ -1,11 +1,32 @@
 # This is dumb but I can't get node-gyp to find these files when its called outside of its directory (i.e. when you yarn install on the root project)
 
+function Extract-File($source, $pattern, $destination) {
+    Add-Type -Assembly System.IO.Compression.FileSystem
+
+    $source = [IO.Path]::Combine($pwd, $source)
+    $destination = [IO.Path]::Combine($pwd, $destination)
+
+    try {
+        $zip = [IO.Compression.ZipFile]::OpenRead($source)
+        $zip.Entries | where {$_.Name -like $pattern} | foreach {[System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, $destination, $true)}
+    }
+    finally {
+        $zip.Dispose()
+    }
+}
+
 if (Test-Path "./flatc" -PathType Leaf) {
+    # use local flatc if avaiable
     $flatc = "./flatc"
 } elseif (Get-Command "flatc" -ErrorAction SilentlyContinue) { 
+    # try global if local is not available
     $flatc = "flatc"
 } else {
-    throw "flatc command not found.";
+    # download and make local flatc available
+    $flatc = "./flatc"
+    Invoke-WebRequest -Uri https://github.com/google/flatbuffers/releases/latest/download/flatc_windows.zip -OutFile ./flatc.zip
+    Extract-File "./flatc.zip" "flatc.exe" "./flatc.exe"
+    Remove-Item "./flatc.zip"
 }
 
 Invoke-Expression "$flatc --cpp --cpp-std c++17 -o output (Get-Item ./*.fbs) --filename-suffix '""""'"
