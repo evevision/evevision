1130a6a7a99fa5f76e7c93eb0b03be8c3d7bcfdb
Jaydubs <dubs@eve.vision>
2020-04-12 17:54:34 -0400

    fix cicd for building dll

--- scripts/build-overlay.ps1
+++ scripts/build-overlay.ps1
@@ -1,6 +1,6 @@
 $vswhere = Join-Path -Path ${env:ProgramFiles(x86)} -ChildPath "Microsoft Visual Studio\Installer\vswhere.exe"
 Write-Host "Building overlay DLL.."
-$msbuild = Invoke-Expression "& '$vswhere' -latest -products Microsoft.VisualStudio.Product.BuildTools -find MSBuild\**\Bin\MSBuild.exe | select-object -first 1"
+$msbuild = Invoke-Expression "& '$vswhere' -latest -products * -find MSBuild\**\Bin\MSBuild.exe | select-object -first 1"
 
 pushd ../overlay
 Invoke-Expression "& '$msbuild' overlay.vcxproj /p:Configuration=Release /p:Platform=x64"
