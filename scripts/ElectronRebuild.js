b169606b12fe427e269ee6391356b7fed32a38fe
Jaydubs <dubs@eve.vision>
2020-04-20 15:43:41 -0400

    fix: correctly build node-pty

--- scripts/ElectronRebuild.js
+++ scripts/ElectronRebuild.js
@@ -3,7 +3,7 @@ import { execSync } from "child_process";
 import fs from "fs";
 import { dependencies } from "../app/package.json";
 
-const nodeModulesPath = path.join(__dirname, "..", "..", "app", "node_modules");
+const nodeModulesPath = path.join(__dirname, "..", "app", "node_modules");
 
 if (
   Object.keys(dependencies || {}).length > 0 &&
@@ -12,7 +12,7 @@ if (
   const electronRebuildCmd =
     "..\\node_modules\\.bin\\electron-rebuild --parallel --force --types prod,dev,optional --module-dir .";
   execSync(electronRebuildCmd, {
-    cwd: path.join(__dirname, "..", "..", "app"),
+    cwd: path.join(__dirname, "..", "app"),
     stdio: "inherit"
   });
 }
